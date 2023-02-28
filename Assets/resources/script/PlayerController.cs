2e7e6f2b06329c12f78d6b2e9fe92f11a02eb9a7
Toan18072001 <manh_quoc_toan@optimizer.co.jp>
2023-02-28 13:15:59 +0700

    update code

--- Assets/resources/script/PlayerController.cs
+++ Assets/resources/script/PlayerController.cs
@@ -6,88 +6,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject bulletPrefab; // Đối tượng viên đạn
-    public List< Transform> firePoints; // Mảng các điểm phát đạn
+    public List<Transform> firePoints; // Mảng các điểm phát đạn
     public float bulletSpeed = 10f; // Tốc độ của viên đạn
     private Vector3 targetPosition; // vị trí mà player muốn đến
     private bool isMoving = false;
     public float speed;
-    public GameObject btnShoot;
-
-    private Vector3 playerPosition;
-
-    void Start()
-    {
-        // Lấy vị trí ban đầu của player
-        playerPosition = transform.position;
-    }
-
+    public float liftBullet;
+    public float timeShoot =2f;
     void Update()
     {
-        if (Application.isMobilePlatform)
+        HandleTouchInput();
+        liftBullet -= Time.deltaTime;
+        if (liftBullet <= 0)
         {
-            // Kiểm tra xem có chạm vào màn hình không
-            if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-            {
-                // Lấy vị trí chạm
-                Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            Shotting();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            // Lấy vị trí của con trỏ chuột trên màn hình
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-                // Di chuyển player theo vị trí chạm
-                playerPosition.x += touchDeltaPosition.x * speed * Time.deltaTime;
-                playerPosition.y += touchDeltaPosition.y * speed * Time.deltaTime;
-            }
-            if (GameManager.Instance.ischeckThreeBullet)
+            // Giới hạn vị trí của con trỏ chuột bên trong màn hình
+            mousePosition.x = Mathf.Clamp(mousePosition.x, -3.44f, 3.44f);
+            mousePosition.y = Mathf.Clamp(mousePosition.y, -4.36f, 4.3f);
+
+            // Chuyển đổi vị trí của con trỏ chuột thành vị trí mới cho player
+            targetPosition = mousePosition;
+            isMoving = true;
+        }
+
+        if (isMoving)
+        {
+            // Di chuyển player đến vị trí mới
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            // Nếu player đã đến vị trí mới, dừng di chuyển
+            if (transform.position == targetPosition)
             {
-                shotThreeBullet();
-            }
-            else
-            {
-                shotBullet();
+                isMoving = false;
             }
         }
-        else
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                if (GameManager.Instance.ischeckThreeBullet)
-                {
-                    shotThreeBullet();
-                }
-                else
-                {
-                    shotBullet();
-                }
-            }
-            if (Input.GetMouseButton(0))
-            {
-                // Lấy vị trí của con trỏ chuột trên màn hình
-                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-                // Giới hạn vị trí của con trỏ chuột bên trong màn hình
-                mousePosition.x = Mathf.Clamp(mousePosition.x, -3.44f, 3.44f);
-                mousePosition.y = Mathf.Clamp(mousePosition.y, -4.36f, 4.3f);
 
-                // Chuyển đổi vị trí của con trỏ chuột thành vị trí mới cho player
-                targetPosition = mousePosition;
-                isMoving = true;
-            }
-
-            if (isMoving)
-            {
-                // Di chuyển player đến vị trí mới
-                transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-
-                // Nếu player đã đến vị trí mới, dừng di chuyển
-                if (transform.position == targetPosition)
-                {
-                    isMoving = false;
-                }
-            }
-        }
-        playerPosition.x = Mathf.Clamp(playerPosition.x, -3.44f, 3.44f);
-        playerPosition.y = Mathf.Clamp(playerPosition.y, -4.36f, 4.36f);
-
-        // Cập nhật vị trí của player
-        transform.position = playerPosition;
     }
 
     private void Shoot(Vector3 position, Vector3 direction)
@@ -107,12 +67,35 @@ public class PlayerController : MonoBehaviour
         Vector2 f2 = (firePoints[2].up - firePoints[2].right) / 2;
         Shoot(firePoints[2].position, f2);
     }
-    private void shotBullet()
+    public void shotBullet()
     {
         Shoot(firePoints[0].position, firePoints[0].up);
     }
-    public void shottingAndroid()
+    void HandleTouchInput()
     {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            // Kiểm tra xem người dùng chạm vào màn hình
+            if (touch.phase == TouchPhase.Began)
+            {
+                // Lấy vị trí của chạm trên màn hình
+                Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+                // Giới hạn vị trí của chạm bên trong màn hình
+                touchPosition.x = Mathf.Clamp(touchPosition.x, -3.44f, 3.44f);
+                touchPosition.y = Mathf.Clamp(touchPosition.y, -4.36f, 4.3f);
+
+                // Chuyển đổi vị trí của chạm thành vị trí mới cho player
+                targetPosition = touchPosition;
+                isMoving = true;
+            }
+        }
+    }
+    private void Shotting()
+    {
+        liftBullet = timeShoot;
         if (GameManager.Instance.ischeckThreeBullet)
         {
             shotThreeBullet();
